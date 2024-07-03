554f33a86adc5cce9a09da48a90685165f1ee329
Muhammad Shayan <shayanhanif73@gmail.com>
2024-07-03 12:08:30 +0500

    updated server and app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   },
 });
